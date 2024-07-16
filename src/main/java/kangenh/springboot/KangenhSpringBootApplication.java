aec6d71f845aae6fc936cc310b494fbdfb126d61
yuda3 <yuda3@hotmail.com>
2024-07-16 16:33:42 +0900

    Initialize SpringBoot Java application with Gradle build config

---
+++ src/main/java/kangenh/springboot/KangenhSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kangenh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangenhSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KangenhSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kangenh/springboot/KangenhSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kangenh.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangenhSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
